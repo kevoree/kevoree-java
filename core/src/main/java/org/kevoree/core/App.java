bae52fefbb1d0a0b76a2efea37d88bd4bf42fcaf
Maxime Tricoire <max.tricoire@gmail.com>
2015-11-05 17:55:26 +0100

    wip

--- core/src/main/java/org/kevoree/core/App.java
+++ core/src/main/java/org/kevoree/core/App.java
@@ -4,10 +4,12 @@ import org.KevoreeModel;
 import org.KevoreeUniverse;
 import org.KevoreeView;
 import org.kevoree.*;
-import org.kevoree.modeling.KCallback;
+import org.kevoree.meta.MetaComponent;
+import org.kevoree.meta.MetaModel;
+import org.kevoree.meta.MetaNode;
+import org.kevoree.meta.MetaOutputPort;
+import org.kevoree.modeling.KObject;
 import org.kevoree.modeling.memory.manager.DataManagerBuilder;
-
-import java.util.Random;
 
 /**
  *
@@ -20,29 +22,118 @@ public class App {
 //        KevoreeCore core = new KevoreeCore();
 //        core.boot("ws://localhost:3080/shared", "node_" + Math.abs(random.nextInt()));
 
+        createTypeModel();
+//        createInstanceModel();
+    }
+
+    private static void createInstanceModel() {
         KevoreeModel kModel = new KevoreeModel(DataManagerBuilder.buildDefault());
         kModel.connect(o -> {
             KevoreeUniverse kUniverse = kModel.universe(0);
             KevoreeView kView = kUniverse.time(System.currentTimeMillis());
             Model model = kView.createModel();
 
-            Namespace ns = kView.createNamespace();
-            ns.setName("kevoree");
+            Node node0 = kView.createNode();
+            node0.setName("node0");
+            model.addNodes(node0);
 
-            model.addNamespaces(ns);
+            Component ticker = kView.createComponent();
+            ticker.setName("ticker");
+            node0.addComponents(ticker);
+
+            OutputPort tickPort = kView.createOutputPort();
+            tickPort.setName("tick");
+            ticker.addOutputs(tickPort);
+
+            Channel chan = kView.createChannel();
+            chan.setName("chan");
+            chan.addOutputs(tickPort);
+            tickPort.addChannels(chan);
+            model.addChannels(chan);
+
+            kView.json().save(model, modelStr -> {
+                kView.json().load(modelStr, loadedModel -> {
+                    System.out.println(loadedModel);
+                    Model newModel = (Model) loadedModel;
+                    newModel.traversal()
+                            .traverse(MetaModel.REL_NODES)
+                            .withAttribute(MetaNode.ATT_NAME, "node0")
+                            .traverse(MetaNode.REL_COMPONENTS)
+                            .withAttribute(MetaComponent.ATT_NAME, "ticker")
+                            .traverse(MetaComponent.REL_OUTPUTS)
+                            .withAttribute(MetaOutputPort.ATT_NAME, "tick")
+                            .then(results -> {
+                                for (KObject res : results) {
+                                    System.out.println(res.toJSON());
+                                }
+                            });
+                });
+            });
+        });
+    }
+
+    private static void createTypeModel() {
+        KevoreeModel kModel = new KevoreeModel(DataManagerBuilder.buildDefault());
+        kModel.connect(o -> {
+            KevoreeUniverse kUniverse = kModel.universe(0);
+            KevoreeView kView = kUniverse.time(System.currentTimeMillis());
+            Model model = kView.createModel();
+
+            Namespace kevoree = kView.createNamespace();
+            kevoree.setName("kevoree");
+            model.addNamespaces(kevoree);
 
             NodeType javaNode = kView.createNodeType();
             javaNode.setName("JavaNode");
             javaNode.setVersion("1");
+            kevoree.addTypeDefinitions(javaNode);
 
             DeployUnit du = kView.createDeployUnit();
             du.setName("kevoree-java-node-java");
             du.setPlatform("java");
             du.setVersion("6.0.0-SNAPSHOT");
-
             javaNode.addDeployUnits(du);
 
-            ns.addTypeDefinitions(javaNode);
+            ComponentType ticker = kView.createComponentType();
+            ticker.setName("Ticker");
+            ticker.setVersion("1");
+            kevoree.addTypeDefinitions(ticker);
+
+            DictionaryType dic = kView.createDictionaryType();
+            ticker.addDictionary(dic);
+
+            AttributeType random = kView.createAttributeType();
+            random.setOptional(false);
+            dic.addAttributes(random);
+
+            BooleanDataType randomType = kView.createBooleanDataType();
+            randomType.setDefault(false);
+            random.addDatatype(randomType);
+
+            AttributeType period = kView.createAttributeType();
+            period.setOptional(true);
+            dic.addAttributes(period);
+
+            IntDataType periodType = kView.createIntDataType();
+            periodType.setDefault(3000);
+            period.addDatatype(periodType);
+
+            PortType tick = kView.createPortType();
+            tick.setName("tick");
+            ticker.addOutputTypes(tick);
+
+            MsgProtocol tickProto = kView.createMsgProtocol();
+            tickProto.setName("jsonschema");
+            Value schema = kView.createValue();
+            schema.setName("schema");
+            schema.setValue("{\"type\": \"number\"}");
+            tickProto.addMetaData(schema);
+            tick.addProtocol(tickProto);
+
+            kView.json().save(model, modelStr -> {
+                System.out.println(modelStr);
+                kModel.disconnect(t -> {});
+            });
         });
     }
 }
--- model/org.kevoree.mm
+++ model/org.kevoree.mm
@@ -1,54 +1,165 @@
 with version "6.0.0-SNAPSHOT"
 
-class org.kevoree.Model {
+class org.kevoree.Model extends org.kevoree.Element {
     rel nodes: org.kevoree.Node
+    rel channels: org.kevoree.Channel
+    rel groups: org.kevoree.Group
 
     rel namespaces: org.kevoree.Namespace
 }
 
-class org.kevoree.Instance {
+class org.kevoree.Element {
+    rel metaData: org.kevoree.Value
+}
+
+class org.kevoree.Instance extends org.kevoree.Element {
     att name: String
 
-    rel typeDefinition: org.kevoree.TypeDefinition
+    rel typeDefinition: org.kevoree.TypeDefinition with maxBound 1
     rel metrics: org.kevoree.Metric
 }
 
 class org.kevoree.Node extends org.kevoree.Instance {
-    rel host: org.kevoree.Node
+    rel host: org.kevoree.Node with maxBound 1
     rel subNodes: org.kevoree.Node
     rel components: org.kevoree.Component
+    rel groups: org.kevoree.Group with opposite "nodes"
+}
+
+class org.kevoree.Channel extends org.kevoree.Instance {
+    rel inputs: org.kevoree.InputPort with opposite "channels"
+    rel outputs: org.kevoree.OutputPort with opposite "channels"
+}
+
+class org.kevoree.Group extends org.kevoree.Instance {
+    rel nodes: org.kevoree.Node with opposite "groups"
 }
 
 class org.kevoree.Component extends org.kevoree.Instance {
-    rel host: org.kevoree.Node
+    rel host: org.kevoree.Node with maxBound 1
+
+    rel inputs: org.kevoree.InputPort with opposite "channels"
+    rel outputs: org.kevoree.OutputPort with opposite "channels"
 }
 
-class org.kevoree.Namespace {
+class org.kevoree.Port extends org.kevoree.Element {
+    att name: String
+
+    rel type: org.kevoree.PortType
+    rel channels: org.kevoree.Channel
+            with opposite "inputs"
+            with opposite "outputs"
+}
+
+class org.kevoree.InputPort extends org.kevoree.Port {}
+class org.kevoree.OutputPort extends org.kevoree.Port {}
+
+class org.kevoree.Namespace extends org.kevoree.Element {
     att name: String
 
     rel typeDefinitions: org.kevoree.TypeDefinition
 }
 
-class org.kevoree.Metric {
+class org.kevoree.Metric extends org.kevoree.Element {
     att name: String
     att value: Continuous
 }
 
-class org.kevoree.TypeDefinition {
+class org.kevoree.TypeDefinition extends org.kevoree.Element {
     att name: String
     att version: String
 
+    rel dictionary: org.kevoree.DictionaryType with maxBound 1
     rel deployUnits: org.kevoree.DeployUnit
 }
 
-class org.kevoree.NodeType extends org.kevoree.TypeDefinition {
+class org.kevoree.NodeType extends org.kevoree.TypeDefinition {}
 
+class org.kevoree.GroupType extends org.kevoree.TypeDefinition {
+    att remote: Bool
+
+    rel fragDictionaries: org.kevoree.FragmentDictionaryType
 }
 
-class org.kevoree.DeployUnit {
+class org.kevoree.ChannelType extends org.kevoree.TypeDefinition {
+    att remote: Bool
+    att fragmentable: Bool
+
+    rel fragDictionaries: org.kevoree.FragmentDictionaryType
+}
+
+class org.kevoree.ComponentType extends org.kevoree.TypeDefinition {
+    att remote: Bool
+
+    rel inputTypes: org.kevoree.PortType
+    rel outputTypes: org.kevoree.PortType
+}
+
+class org.kevoree.DeployUnit extends org.kevoree.Element {
     att name: String
     att platform: String
     att version: String
 
     rel dependencies: org.kevoree.DeployUnit
+}
+
+class org.kevoree.DictionaryType extends org.kevoree.Element {
+    rel attributes: org.kevoree.AttributeType
+}
+
+class org.kevoree.FragmentDictionaryType extends org.kevoree.Dictionary {
+    rel fragment: org.kevoree.Node
+}
+
+class org.kevoree.AttributeType extends org.kevoree.Element {
+    att optional: Bool
+
+    rel datatype: org.kevoree.DataType with maxBound 1
+}
+
+class org.kevoree.PortType extends org.kevoree.Element {
+    att name: String
+
+    rel protocol: org.kevoree.MsgProtocol
+}
+
+class org.kevoree.Value extends org.kevoree.Element {
+    att name: String
+    att value: String
+}
+
+class org.kevoree.MsgProtocol extends org.kevoree.Element {
+    att name: String
+}
+
+class org.kevoree.DataType extends org.kevoree.Element {}
+class org.kevoree.StringDataType extends org.kevoree.DataType {
+    att multiline: Bool
+    att default: String
+}
+class org.kevoree.DoubleDataType extends org.kevoree.DataType {
+    att min: Double
+    att max: Double
+    att default: Double
+}
+class org.kevoree.IntDataType extends org.kevoree.DataType {
+    att min: Int
+    att max: Int
+    att default: Int
+}
+class org.kevoree.LongDataType extends org.kevoree.DataType {
+    att min: Long
+    att max: Long
+    att default: Long
+}
+class org.kevoree.BooleanDataType extends org.kevoree.DataType {
+    att default: Bool
+}
+class org.kevoree.ChoiceDataType extends org.kevoree.DataType {
+    rel default: org.kevoree.Choice with maxBound 1
+    rel choices: org.kevoree.Choice
+}
+
+class org.kevoree.Choice {
+    att value: String
 }
