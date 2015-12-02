c647a1f738a7fc802cb19d5687a2e10e329aa989
Maxime Tricoire <max.tricoire@gmail.com>
2015-12-02 11:45:58 +0100

    using port mocking for Ticker

--- library/comp/ticker/src/test/java/org/kevoree/library/TickerTest.java
+++ library/comp/ticker/src/test/java/org/kevoree/library/TickerTest.java
@@ -1,59 +1,34 @@
 package org.kevoree.library;
 
-import org.easymock.EasyMock;
-import org.easymock.EasyMockRunner;
-import org.easymock.Mock;
-import org.easymock.TestSubject;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.kevoree.annotations.params.IntParam;
-import org.kevoree.api.OutputPort;
 import org.kevoree.test.MockComponent;
-import org.kevoree.test.exception.CreateMockException;
-import org.kevoree.test.exception.SetParamException;
-
-import java.lang.reflect.Field;
-import java.util.Collections;
-import java.util.concurrent.ScheduledExecutorService;
 
 /**
  *
  * Created by leiko on 11/30/15.
  */
-@RunWith(EasyMockRunner.class)
 public class TickerTest {
 
-    @TestSubject
+    private MockComponent<Ticker> mock;
     private Ticker ticker;
 
-    @Mock
-    private OutputPort tick;
-
-    @Mock
-    private ScheduledExecutorService executor;
-
-    public TickerTest() throws Exception {
-        MockComponent<Ticker> mock = new MockComponent<>(Ticker.class).setParam("period", 1);
-        //System.out.println(mock.toString());
-        ticker = mock.get();
+    @Before
+    public void setUp() throws Exception {
+        mock = new MockComponent<>(Ticker.class);
+        ticker = mock.setParam("period", 100).get();
     }
 
     @Test
-    public void testStart() throws InterruptedException, NoSuchFieldException, IllegalAccessException {
-        tick.send(EasyMock.anyString());
-        EasyMock.replay(tick);
-
+    public void testStart() throws Exception {
+        mock.expectPort("tick").toBeCalled(1).async();
         ticker.start();
-        Thread.sleep(2);
-        EasyMock.verify(tick);
+        mock.verifyPorts();
     }
 
-    @Test
-    public void testStop() throws InterruptedException {
-        EasyMock.expect(executor.shutdownNow()).andReturn(Collections.emptyList());
-        EasyMock.replay(executor);
-
-        ticker.stop();
-        EasyMock.verify(executor);
+    @Test(expected = IllegalArgumentException.class)
+    public void testWrongPeriod() throws Exception {
+        ticker = mock.setParam("period", 0).get();
+        ticker.start();
     }
 }
